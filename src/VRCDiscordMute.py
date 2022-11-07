e20f307ea367a2949a93940c2551fd905812767f
Isaak <43730681+I5UCC@users.noreply.github.com>
2022-11-07 19:08:39 +0100

    Release key on exit

--- src/VRCDiscordMute.py
+++ src/VRCDiscordMute.py
@@ -5,6 +5,7 @@ from pythonosc import osc_server
 import openvr
 import os
 import sys
+import atexit
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -23,9 +24,13 @@ def handle_mute_event(addr, value):
   else:
     keyboard.release(config["config"]["PushMuteKey"])
 
+def exit_handler():
+  keyboard.release(config["config"]["PushMuteKey"])
+
 dispatcher = dispatcher.Dispatcher()
 dispatcher.map("/avatar/parameters/MuteSelf", handle_mute_event)
 
 server = osc_server.ThreadingOSCUDPServer((config["config"]["IP"], int(config["config"]["Port"])), dispatcher)
 print(f"Listening to {server.server_address}\nPushMuteKey: {config['config']['PushMuteKey']}")
+atexit.register(exit_handler)
 server.serve_forever()
