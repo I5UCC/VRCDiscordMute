78fc16cf9ff3c664392f15371bcf75e3a3246bcd
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-01-12 03:33:25 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,19 +1,16 @@
-import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
 # "packages": ["os"] is used as example only
 packages = ["pythonosc", "configparser", "keyboard"]
-exclude = ["tkinter", "html", "http", "lib2to3", "multiprocessing", "test", "unittest", "xmlrpc", "os", "traceback", "sys", "time", "argparse"]
 file_include = ["config.ini", "app.vrmanifest"]
-bin_excludes = ["_bz2.pyd", "_decimal.pyd", "_hashlib.pyd", "_lzma.pyd", "_queue.pyd", "_ssl.pyd", "libcrypto-1_1.dll", "libssl-1_1.dll", "ucrtbase.dll", "VCRUNTIME140.dll"]
 
-build_exe_options = {"packages": packages, "excludes": exclude, "include_files": file_include, "bin_excludes": bin_excludes}
+build_exe_options = {"packages": packages, "include_files": file_include}
 
 setup(
-    name="ThumbParamsOSC",
-    version="0.3.1",
-    description="ThumbParamsOSC",
+    name="VRCDiscordMute",
+    version="0.2",
+    description="VRCDiscordMute",
     options={"build_exe": build_exe_options},
     executables=[Executable("VRCDiscordMute.py", targetName="VRCDiscordMute.exe", base=False), Executable("VRCDiscordMute.py", targetName="VRCDiscordMute_NoConsole.exe", base="Win32GUI"), Executable("GetHotkeyName.py", targetName="GetHotkeyName.exe", base=False)],
 )
